3a725deaf8ef5807a07a5927e883cda1a6fec173
tolmachdr <113459358+tolmachdr@users.noreply.github.com>
2023-07-02 21:05:11 +0600

    Initial Commit

---
+++ Swift.cource.hw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift.cource.hw
+//
+//  Created by Kulbatchayev Timyr on 02.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
